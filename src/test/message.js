75a7f91672cde43b3a8b094a7fec8dd94a0e4650
Jonathan Warner <jewarner57@gmail.com>
2021-04-22 20:12:28 -0400

    feat: complete all message routes and test routes

--- src/test/message.js
+++ src/test/message.js
@@ -25,40 +25,117 @@ after((done) => {
   done()
 })
 
+const SAMPLE_OBJECT_ID = 'aaaaaaaaaaaa' // 12 byte string
 
 describe('Message API endpoints', () => {
-    beforeEach((done) => {
-        // TODO: add any beforeEach code here
-        done()
+
+  beforeEach((done) => {
+    const sampleUser = new User({
+      username: 'myuser',
+      password: 'mypassword',
+      _id: SAMPLE_OBJECT_ID
     })
 
-    afterEach((done) => {
-        // TODO: add any afterEach code here
-        done()
+    const sampleMessage = new Message({
+      title: 'my title',
+      body: 'my body',
+      author: sampleUser._id,
+      _id: SAMPLE_OBJECT_ID
     })
 
-    it('should load all messages', (done) => {
-        // TODO: Complete this
-        done()
-    })
+    sampleUser.save()
+    sampleMessage.save()
 
-    it('should get one specific message', (done) => {
-        // TODO: Complete this
-        done()
-    })
+    done()
+  })
 
-    it('should post a new message', (done) => {
-        // TODO: Complete this
-        done()
-    })
+  afterEach((done) => {
 
-    it('should update a message', (done) => {
-        // TODO: Complete this
-        done()
-    })
+    Message.deleteMany({ title: ['my title', 'new msg title', 'another title'] })
 
-    it('should delete a message', (done) => {
-        // TODO: Complete this
+      .then(() => {
+
+        User.deleteMany({ username: ['myuser'] })
+          .then(() => {
+            done()
+          })
+
+      })
+  })
+
+  it('should load all messages', (done) => {
+    chai.request(app)
+      .get('/messages')
+      .end((err, res) => {
+        if (err) { done(err) }
+        expect(res).to.have.status(200)
+        expect(res.body.messages).to.be.an("array")
+
         done()
-    })
+      })
+  })
+
+  it('should get one specific message', (done) => {
+    chai.request(app)
+      .get(`/messages/${SAMPLE_OBJECT_ID}`)
+      .end((err, res) => {
+        if (err) { done(err) }
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.an('object')
+        expect(res.body.title).to.equal('my title')
+        expect(res.body.body).to.equal('my body')
+        done()
+      })
+  })
+
+  it('should post a new message', (done) => {
+    chai.request(app)
+      .post('/messages')
+      .send({ title: 'new msg title', body: 'new msg body', author: SAMPLE_OBJECT_ID })
+      .end((err, res) => {
+        if (err) { done(err) }
+        expect(res.body).to.be.an('object')
+        expect(res.body).to.have.property('title', 'new msg title')
+        expect(res.body).to.have.property('body', 'new msg body')
+
+        // check that message is actually inserted into database
+        Message.findOne({ title: 'new msg title' }).then(message => {
+          expect(message).to.be.an('object')
+          done()
+        })
+      })
+  })
+
+  it('should update a message', (done) => {
+    chai.request(app)
+      .put(`/messages/${SAMPLE_OBJECT_ID}`)
+      .send({ title: 'another title' })
+      .end((err, res) => {
+        if (err) { done(err) }
+        expect(res.body.message).to.be.an('object')
+        expect(res.body.message).to.have.property('title', 'another title')
+
+        // check that a message is actually inserted into database
+        Message.findOne({ title: 'another title' }).then(message => {
+          expect(message).to.be.an('object')
+          done()
+        })
+      })
+  })
+
+  it('should delete a message', (done) => {
+    chai.request(app)
+      .delete(`/messages/${SAMPLE_OBJECT_ID}`)
+      .end((err, res) => {
+        if (err) { done(err) }
+        expect(res.body.message).to.equal('Successfully deleted.')
+        expect(res.body._id).to.equal(SAMPLE_OBJECT_ID)
+
+        // check that user is actually deleted from database
+        Message.findOne({ title: 'my title' }).then(message => {
+          expect(message).to.equal(null)
+          done()
+        })
+      })
+  })
 })
